8e186f1f55a3e6eefbcd40694b029d973b252f0c
Samuel <samuel@example.com>
2026-02-14 07:15:21 -0800

    good

--- tests/test_smokeside.py
+++ tests/test_smokeside.py
@@ -3,6 +3,7 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions
@@ -12,7 +13,9 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class TestSmokeside():
   def setup_method(self, method):
-    self.driver = webdriver.Firefox()
+    options = Options()
+    options.add_argument("--headless=new")
+    self.driver = webdriver.Chrome(options=options)
     self.vars = {}
   
   def teardown_method(self, method):
